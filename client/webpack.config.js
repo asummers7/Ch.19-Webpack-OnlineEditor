320d9361127a0ffd2b8ae25e979d11dd20dd472a
Alec S <asummers34@gmail.com>
2023-03-10 22:01:48 -0700

    fixing the install button

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = () => {
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'server-worker.js'
+        swDest: 'src-sw.js'
       }),
       new WebpackPwaManifest({
         short_name: "JATE",
